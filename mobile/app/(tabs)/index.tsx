18bad17a05cd9037a29236dfe3b868ccd471abaa
AnzeFric <anzeifric@gmail.com>
2025-02-17 17:00:22 +0100

    index: Added fonts

--- mobile/app/(tabs)/index.tsx
+++ mobile/app/(tabs)/index.tsx
@@ -1,9 +1,18 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
 export default function HomeScreen() {
   return (
     <View>
-      <Text>Home screen</Text>
+      <Text style={styles.userGreeting}>Welcome User!</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  userGreeting: {
+    fontSize: 36,
+    paddingTop: 20,
+    paddingStart: 25,
+    fontFamily: "Jaldi-Regular",
+  },
+});
--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -6,9 +6,25 @@ import {
 import { Stack } from "expo-router";
 import { useColorScheme } from "@/hooks/useColorScheme";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import * as Font from "expo-font";
+import { useEffect, useState } from "react";
+
+const loadFonts = async () => {
+  await Font.loadAsync({
+    "Jaldi-Regular": require("@/assets/fonts/Jaldi-Regular.ttf"),
+    "Jaldi-Bold": require("@/assets/fonts/Jaldi-Bold.ttf"),
+  });
+};
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+
+  useEffect(() => {
+    loadFonts().then(() => setFontsLoaded(true));
+  }, []);
+
+  if (!fontsLoaded) return null;
 
   return (
     <SafeAreaProvider>
