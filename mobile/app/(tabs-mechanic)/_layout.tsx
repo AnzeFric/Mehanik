97ef724a5c0d24884d43bc5206496c5a647c149c
AnzeFric <anzeifric@gmail.com>
2025-02-24 09:53:06 +0100

    Tab layout on mechanic side and fixed icons

--- mobile/app/(tabs-mechanic)/_layout.tsx
+++ mobile/app/(tabs-mechanic)/_layout.tsx
@@ -1,22 +1,70 @@
-import { Stack } from "expo-router";
+import { Tabs } from "expo-router";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Colors } from "@/constants/Colors";
+import { View } from "react-native";
+import HomeIcon from "@/assets/icons/HomeIcon.svg";
+import AppointmentsIcon from "@/assets/icons/mechanic-tabs/AppointmentIcon.svg";
+import LibraryIcon from "@/assets/icons/mechanic-tabs/LibraryIcon.svg";
 
 export default function TabLayout() {
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      <Stack
-        screenOptions={{
-          contentStyle: { backgroundColor: Colors.light.background },
+      <Tabs
+        screenOptions={({ route }) => ({
+          tabBarActiveTintColor: Colors["light"].tint,
           headerShown: false,
-        }}
+          tabBarShowLabel: false,
+          tabBarStyle: {
+            height: 64,
+          },
+        })}
+        backBehavior="history"
       >
-        <Stack.Screen name="index" />
-        <Stack.Screen name="settings" />
-        <Stack.Screen name="library" />
-        <Stack.Screen name="appointments" />
-      </Stack>
+        <Tabs.Screen
+          name="index"
+          options={{
+            title: "Home",
+            tabBarIcon: ({ color, focused }) => (
+              <View style={{ margin: 20, padding: 20 }}>
+                <HomeIcon color={Colors.light.tint} width={28} height={28} />
+              </View>
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="appointments"
+          options={{
+            title: "Appointments",
+            tabBarIcon: ({ color, focused }) => (
+              <View style={{ margin: 20, padding: 20 }}>
+                <AppointmentsIcon
+                  color={Colors.light.tint}
+                  width={28}
+                  height={28}
+                />
+              </View>
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="library"
+          options={{
+            title: "Library",
+            tabBarIcon: ({ color, focused }) => (
+              <View style={{ margin: 20, marginTop: 23, padding: 20 }}>
+                <LibraryIcon color={Colors.light.tint} width={28} height={28} />
+              </View>
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="settings"
+          options={{
+            href: null,
+          }}
+        />
+      </Tabs>
     </SafeAreaView>
   );
 }
