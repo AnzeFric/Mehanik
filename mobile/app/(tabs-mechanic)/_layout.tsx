16d2e91c11f0a76170a349b4befc0f8b448138fb
AnzeFric <anzeifric@gmail.com>
2025-02-25 13:01:47 +0100

    Fixed icons to accept custom colors, made tab icons display current/active screen with different colors

--- mobile/app/(tabs-mechanic)/_layout.tsx
+++ mobile/app/(tabs-mechanic)/_layout.tsx
@@ -1,4 +1,4 @@
-import { Tabs, router } from "expo-router";
+import { Tabs, usePathname } from "expo-router";
 import React from "react";
 import { Pressable } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
@@ -9,16 +9,22 @@ import AppointmentsIcon from "@/assets/icons/mechanic-tabs/AppointmentIcon.svg";
 import LibraryIcon from "@/assets/icons/mechanic-tabs/LibraryIcon.svg";
 
 export default function TabLayout() {
+  const pathName = usePathname();
+
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <Tabs
-        screenOptions={({ route }) => ({
+        screenOptions={() => ({
           headerShown: false,
           tabBarShowLabel: false,
+          tabBarActiveTintColor: Colors.light.specialBlue,
+          tabBarInactiveTintColor: Colors.light.inactiveButton,
           tabBarStyle: {
             height: 64,
           },
           sceneStyle: { backgroundColor: Colors.light.background },
+
+          // Disables android default onClick ripple effect
           tabBarButton: (props) => (
             <Pressable
               {...props}
@@ -32,11 +38,17 @@ export default function TabLayout() {
           name="index"
           options={{
             title: "Home",
-            tabBarActiveTintColor: "#FFFFFF",
-            tabBarInactiveTintColor: "#FFFFFF",
-            tabBarIcon: ({ color, focused }) => (
+            tabBarIcon: ({ color }) => (
               <View style={{ margin: 20, padding: 20 }}>
-                <HomeIcon width={28} height={28} />
+                <HomeIcon
+                  color={
+                    pathName.startsWith("/settings") // Color HomeIcon as active if we are on settings page
+                      ? Colors.light.specialBlue
+                      : color
+                  }
+                  width={28}
+                  height={28}
+                />
               </View>
             ),
           }}
@@ -45,21 +57,20 @@ export default function TabLayout() {
           name="appointments"
           options={{
             title: "Appointments",
-            tabBarIcon: ({ color, focused }) => (
+            tabBarIcon: ({ color }) => (
               <View style={{ margin: 20, padding: 20 }}>
-                <AppointmentsIcon width={28} height={28} />
+                <AppointmentsIcon color={color} width={28} height={28} />
               </View>
             ),
-            headerPressColor: "white",
           }}
         />
         <Tabs.Screen
           name="library"
           options={{
             title: "Library",
-            tabBarIcon: ({ color, focused }) => (
+            tabBarIcon: ({ color }) => (
               <View style={{ margin: 20, marginTop: 23, padding: 20 }}>
-                <LibraryIcon width={28} height={28} />
+                <LibraryIcon color={color} width={28} height={28} />
               </View>
             ),
           }}
