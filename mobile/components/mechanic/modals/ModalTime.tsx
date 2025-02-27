95e0f448743e08f59fe011410899f61db769f2a7
AnzeFric <anzeifric@gmail.com>
2025-02-27 08:02:31 +0100

    Prepared modal files

---
+++ mobile/components/mechanic/modals/ModalTime.tsx
@@ -0,0 +1,49 @@
+import { View, Modal, StyleSheet } from "react-native";
+import { Colors } from "@/constants/Colors";
+
+interface Props {
+  isVisible: boolean;
+  message: string;
+  onConfirm: () => void;
+  onCancel: () => void;
+}
+
+export default function ModalTime({
+  isVisible,
+  message,
+  onConfirm,
+  onCancel,
+}: Props) {
+  return (
+    <Modal transparent={true} visible={isVisible} onRequestClose={onCancel}>
+      <View style={styles.centeredView}>
+        <View style={styles.modalView}></View>
+      </View>
+    </Modal>
+  );
+}
+
+const styles = StyleSheet.create({
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+  },
+  modalView: {
+    padding: 35,
+    backgroundColor: Colors.light.background,
+    borderRadius: 20,
+    alignItems: "center",
+    shadowColor: Colors.light.shadowColor,
+    width: "75%",
+
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+});
