104216a64672af0d78390c793dbc609689d2215c
AnzeFric <anzeifric@gmail.com>
2025-05-16 09:38:04 +0200

    Backend: Added Supabase connection

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,13 @@ const app = express();
 
 app.use(express.json());
 
+// Supabase client
+const { createClient } = require("@supabase/supabase-js");
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_ANON_KEY;
+
+const supabase = createClient(supabaseUrl, supabaseKey);
+
 app.listen(3000, () => {
   console.log("Mehanik backend started successfully!");
 });
