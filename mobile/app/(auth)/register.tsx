bd13ef67938a6a263ffe7b35089d2efaec82364a
AnzeFric <anzeifric@gmail.com>
2025-05-20 16:27:50 +0200

    Frontend, register: UI update

--- mobile/app/(auth)/register.tsx
+++ mobile/app/(auth)/register.tsx
@@ -4,6 +4,7 @@ import {
   TextInput,
   TouchableOpacity,
   TouchableHighlight,
+  ScrollView,
   StyleSheet,
 } from "react-native";
 import { useState } from "react";
@@ -16,100 +17,112 @@ import { AccountType } from "@/interfaces/account";
 export default function LoginScreen() {
   const { handleRegister } = useAuth();
   const [email, setEmail] = useState("");
-  const [name, setName] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
   const [accountType, setAccountType] = useState<AccountType>("user");
 
   return (
-    <View style={[AppStyles.parentPadding, styles.container]}>
-      <View style={styles.headerContainer}>
-        <Text style={styles.title}>Ustvarite račun</Text>
-        <Text style={styles.subtitle}>
-          Izberite vrsto računa in izpolnite podatke
-        </Text>
-      </View>
+    <View style={styles.container}>
+      <ScrollView style={AppStyles.parentPadding}>
+        <View style={styles.headerContainer}>
+          <Text style={styles.title}>Ustvarite račun</Text>
+          <Text style={styles.subtitle}>
+            Izberite vrsto računa in izpolnite podatke
+          </Text>
+        </View>
 
-      <View style={{ gap: 20 }}>
-        <TextInput
-          style={AppStyles.textInput}
-          placeholder={"Ime"}
-          value={name}
-          onChangeText={setName}
-          autoCapitalize={"words"}
-          autoComplete={"off"}
-        />
+        <View style={{ gap: 20 }}>
+          <TextInput
+            style={AppStyles.textInput}
+            placeholder={"Ime"}
+            value={firstName}
+            onChangeText={setFirstName}
+            autoCapitalize={"words"}
+            autoComplete={"off"}
+          />
 
-        <TextInput
-          style={AppStyles.textInput}
-          placeholder={"Email"}
-          value={email}
-          onChangeText={setEmail}
-          keyboardType={"email-address"}
-          autoCapitalize={"none"}
-          autoComplete={"off"}
-        />
+          <TextInput
+            style={AppStyles.textInput}
+            placeholder={"Priimek"}
+            value={lastName}
+            onChangeText={setLastName}
+            autoCapitalize={"words"}
+            autoComplete={"off"}
+          />
 
-        <TextInput
-          style={AppStyles.textInput}
-          placeholder={"Geslo"}
-          value={password}
-          onChangeText={setPassword}
-          autoComplete={"off"}
-          secureTextEntry
-        />
+          <TextInput
+            style={AppStyles.textInput}
+            placeholder={"Email"}
+            value={email}
+            onChangeText={setEmail}
+            keyboardType={"email-address"}
+            autoCapitalize={"none"}
+            autoComplete={"off"}
+          />
 
-        <TextInput
-          style={AppStyles.textInput}
-          placeholder={"Ponovite geslo"}
-          value={confirmPassword}
-          onChangeText={setConfirmPassword}
-          autoComplete={"off"}
-          secureTextEntry
-        />
+          <TextInput
+            style={AppStyles.textInput}
+            placeholder={"Geslo"}
+            value={password}
+            onChangeText={setPassword}
+            autoComplete={"off"}
+            secureTextEntry
+          />
 
-        <View style={{ gap: 10 }}>
-          <Text>Kdo ste?</Text>
-          <View style={styles.userTypeContainer}>
-            <TouchableOpacity
-              style={[
-                styles.userTypeButton,
-                accountType === "user" && styles.selectedButton,
-              ]}
-              onPress={() => setAccountType("user")}
-            >
-              <Text style={styles.userTypeText}>Sem stranka</Text>
-            </TouchableOpacity>
+          <TextInput
+            style={AppStyles.textInput}
+            placeholder={"Ponovite geslo"}
+            value={confirmPassword}
+            onChangeText={setConfirmPassword}
+            autoComplete={"off"}
+            secureTextEntry
+          />
 
-            <TouchableOpacity
-              style={[
-                styles.userTypeButton,
-                accountType === "mechanic" && styles.selectedButton,
-              ]}
-              onPress={() => setAccountType("mechanic")}
-            >
-              <Text style={styles.userTypeText}>Sem avtomehanik</Text>
-            </TouchableOpacity>
+          <View style={{ gap: 10 }}>
+            <Text>Kdo ste?</Text>
+            <View style={styles.userTypeContainer}>
+              <TouchableOpacity
+                style={[
+                  styles.userTypeButton,
+                  accountType === "user" && styles.selectedButton,
+                ]}
+                onPress={() => setAccountType("user")}
+              >
+                <Text style={styles.userTypeText}>Sem stranka</Text>
+              </TouchableOpacity>
+
+              <TouchableOpacity
+                style={[
+                  styles.userTypeButton,
+                  accountType === "mechanic" && styles.selectedButton,
+                ]}
+                onPress={() => setAccountType("mechanic")}
+              >
+                <Text style={styles.userTypeText}>Sem avtomehanik</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+
+          <TouchableHighlight
+            style={AppStyles.button}
+            onPress={handleRegister}
+            underlayColor={Colors.light.specialBlueClick}
+          >
+            <Text style={AppStyles.buttonText}>Registracija</Text>
+          </TouchableHighlight>
+
+          <View style={styles.loginContainer}>
+            <Text style={styles.loginText}>Že imate račun? </Text>
+            <Link href="/(auth)/login" asChild>
+              <TouchableOpacity>
+                <Text style={styles.loginLink}>Prijava</Text>
+              </TouchableOpacity>
+            </Link>
           </View>
         </View>
-
-        <TouchableHighlight
-          style={AppStyles.button}
-          onPress={handleRegister}
-          underlayColor={Colors.light.specialBlueClick}
-        >
-          <Text style={AppStyles.buttonText}>Registracija</Text>
-        </TouchableHighlight>
-
-        <View style={styles.loginContainer}>
-          <Text style={styles.loginText}>Že imate račun? </Text>
-          <Link href="/(auth)/login" asChild>
-            <TouchableOpacity>
-              <Text style={styles.loginLink}>Prijava</Text>
-            </TouchableOpacity>
-          </Link>
-        </View>
-      </View>
+      </ScrollView>
     </View>
   );
 }
@@ -119,12 +132,11 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   headerContainer: {
-    paddingVertical: 60,
+    paddingVertical: 35,
   },
   title: {
     fontSize: 44,
     fontWeight: "bold",
-    lineHeight: 44,
   },
   subtitle: {
     fontSize: 16,
