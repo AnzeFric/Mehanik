f84f7557336068ac48ebebc628d7ad726fd04618
AnzeFric <anzeifric@gmail.com>
2025-06-12 22:07:58 +0200

    Frontend, colorTheme: user, Mechanic item finished

--- mobile/components/user/items/Mechanic.tsx
+++ mobile/components/user/items/Mechanic.tsx
@@ -1,84 +1,77 @@
-import { View, Text, Image, TouchableOpacity, StyleSheet } from "react-native";
+import { View, Image, TouchableOpacity, StyleSheet } from "react-native";
 import { router } from "expo-router";
-import { Colors } from "@/constants/Colors";
 import { MechanicData } from "@/interfaces/user";
+import ThemedView from "@/components/global/themed/ThemedView";
+import ThemedText from "@/components/global/themed/ThemedText";
+import { useAnimatedTheme } from "@/hooks/useAnimatedTheme";
 
 interface Props {
   mechanicData: MechanicData;
 }
 
 export default function Mechanic({ mechanicData }: Props) {
+  const { staticColors } = useAnimatedTheme();
+
   const handlePress = () => {
     router.push(`/(tabs-user)/mechanics/mechanic/${mechanicData.email}`);
   };
 
   return (
-    <TouchableOpacity
-      style={styles.container}
-      activeOpacity={0.7}
-      onPress={handlePress}
-    >
-      {mechanicData.info.image ? (
-        <Image source={{ uri: mechanicData.info.image }} style={styles.image} />
-      ) : (
-        <Image
-          source={require("@/assets/images/logo-main.png")}
-          style={styles.image}
-        />
-      )}
-      <View style={styles.infoContainer}>
-        <Text style={styles.name}>
-          {mechanicData.firstName} {mechanicData.lastName}
-        </Text>
-        {mechanicData.info.address && (
-          <Text style={styles.address}>
-            {mechanicData.info.address}, {mechanicData.info.city}
-          </Text>
+    <ThemedView type={"secondary"}>
+      <TouchableOpacity
+        style={[
+          styles.container,
+          { borderLeftColor: staticColors.specialBlue },
+        ]}
+        activeOpacity={0.7}
+        onPress={handlePress}
+      >
+        {mechanicData.info.image ? (
+          <Image
+            source={{ uri: mechanicData.info.image }}
+            style={styles.image}
+          />
+        ) : (
+          <Image
+            source={require("@/assets/images/logo-main.png")}
+            style={styles.image}
+          />
         )}
-        {mechanicData.info.phone && (
-          <Text style={styles.contact}>{mechanicData.info.phone}</Text>
-        )}
-        {mechanicData.email && (
-          <Text style={styles.contact}>{mechanicData.email}</Text>
-        )}
-      </View>
-    </TouchableOpacity>
+        <View style={{ flex: 1 }}>
+          <ThemedText type={"small"} bold>
+            {mechanicData.firstName} {mechanicData.lastName}
+          </ThemedText>
+          {mechanicData.info.address && (
+            <ThemedText type={"extraSmall"}>
+              {mechanicData.info.address}, {mechanicData.info.city}
+            </ThemedText>
+          )}
+
+          {mechanicData.info.phone && (
+            <ThemedText type={"extraSmall"}>
+              {mechanicData.info.phone}
+            </ThemedText>
+          )}
+          {mechanicData.email && (
+            <ThemedText type={"extraSmall"}>{mechanicData.email}</ThemedText>
+          )}
+        </View>
+      </TouchableOpacity>
+    </ThemedView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flexDirection: "row",
-    backgroundColor: "#fff",
     padding: 15,
     borderLeftWidth: 6,
-    borderLeftColor: Colors.light.specialBlue,
-    shadowColor: "#000",
-    shadowOpacity: 0.1,
-    shadowRadius: 5,
-    elevation: 3,
-    marginBottom: 10,
     alignItems: "center",
+    gap: 15,
   },
   image: {
     width: 70,
     height: 70,
     borderRadius: 6,
-    marginRight: 15,
-  },
-  infoContainer: {
-    flex: 1,
-  },
-  name: {
-    fontSize: 16,
-    fontWeight: "bold",
-  },
-  address: {
-    fontSize: 14,
-    color: "#666",
-  },
-  contact: {
-    fontSize: 14,
-    color: "#888",
   },
 });
