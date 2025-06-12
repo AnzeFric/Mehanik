d4c6640eefb132b2bcf22c684541b42b65ebfedf
AnzeFric <anzeifric@gmail.com>
2025-06-12 13:18:53 +0200

    Frontend, colorTheme: Customer and PlusButton finished

--- mobile/components/global/PlusButton.tsx
+++ mobile/components/global/PlusButton.tsx
@@ -1,14 +1,23 @@
 import { StyleSheet, TouchableOpacity } from "react-native";
 import PlusIcon from "@/assets/icons/PlusIcon.svg";
 import { Colors } from "@/constants/Colors";
+import { useAnimatedTheme } from "@/hooks/useAnimatedTheme";
 
 interface Props {
   onPress: () => void;
 }
 
 export default function PlusButton({ onPress }: Props) {
+  const { staticColors } = useAnimatedTheme();
+
   return (
-    <TouchableOpacity style={styles.fixedAddButton} onPress={onPress}>
+    <TouchableOpacity
+      style={[
+        styles.fixedAddButton,
+        { backgroundColor: staticColors.specialBlue },
+      ]}
+      onPress={onPress}
+    >
       <PlusIcon height={35} width={35} color={Colors.light.background} />
     </TouchableOpacity>
   );
@@ -19,7 +28,6 @@ const styles = StyleSheet.create({
     position: "absolute",
     bottom: 20,
     right: 20,
-    backgroundColor: Colors.light.specialBlue,
     borderRadius: 30,
     width: 60,
     height: 60,
--- mobile/components/mechanic/items/Customer.tsx
+++ mobile/components/mechanic/items/Customer.tsx
@@ -1,13 +1,17 @@
-import { View, Text, Image, TouchableOpacity, StyleSheet } from "react-native";
+import { View, Image, TouchableOpacity, StyleSheet } from "react-native";
 import { router } from "expo-router";
-import { Colors } from "@/constants/Colors";
 import { CustomerVehicleData } from "@/interfaces/customer";
+import ThemedView from "@/components/global/themed/ThemedView";
+import ThemedText from "@/components/global/themed/ThemedText";
+import { useAnimatedTheme } from "@/hooks/useAnimatedTheme";
 
 interface Props {
   customerData: CustomerVehicleData;
 }
 
 export default function Customer({ customerData }: Props) {
+  const { staticColors } = useAnimatedTheme();
+
   const handlePress = () => {
     router.push(
       `/library/customer/${customerData.vehicle.uuid}?firstName=${customerData.customer.firstName}`
@@ -15,74 +19,59 @@ export default function Customer({ customerData }: Props) {
   };
 
   return (
-    <TouchableOpacity
-      style={styles.container}
-      activeOpacity={0.7}
-      onPress={handlePress}
-    >
-      {customerData.vehicle.image ? (
-        <Image
-          source={{ uri: customerData.vehicle.image }}
-          style={styles.image}
-        />
-      ) : (
-        <Image
-          source={require("@/assets/images/logo-main.png")}
-          style={styles.image}
-        />
-      )}
-      <View style={styles.infoContainer}>
-        <Text style={styles.name}>
-          {customerData.customer.firstName} {customerData.customer.lastName}
-        </Text>
-        <Text style={styles.address}>
-          {customerData.vehicle.brand} {customerData.vehicle.model}
-          {customerData.vehicle.buildYear && (
-            <>
-              {", "}
-              {customerData.vehicle.buildYear}
-            </>
-          )}
-        </Text>
-        <Text style={styles.contact}>{customerData.vehicle.vin}</Text>
-      </View>
-    </TouchableOpacity>
+    <ThemedView type={"secondary"}>
+      <TouchableOpacity
+        style={[
+          styles.container,
+          { borderLeftColor: staticColors.specialBlue },
+        ]}
+        activeOpacity={0.7}
+        onPress={handlePress}
+      >
+        {customerData.vehicle.image ? (
+          <Image
+            source={{ uri: customerData.vehicle.image }}
+            style={styles.image}
+          />
+        ) : (
+          <Image
+            source={require("@/assets/images/logo-main.png")}
+            style={styles.image}
+          />
+        )}
+        <View style={{ flex: 1 }}>
+          <ThemedText type={"small"} bold>
+            {customerData.customer.firstName} {customerData.customer.lastName}
+          </ThemedText>
+          <ThemedText type={"extraSmall"}>
+            {customerData.vehicle.brand} {customerData.vehicle.model}
+            {customerData.vehicle.buildYear && (
+              <>
+                {", "}
+                {customerData.vehicle.buildYear}
+              </>
+            )}
+          </ThemedText>
+          <ThemedText type={"extraSmall"}>
+            {customerData.vehicle.vin}
+          </ThemedText>
+        </View>
+      </TouchableOpacity>
+    </ThemedView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flexDirection: "row",
-    backgroundColor: "#fff",
     padding: 15,
     borderLeftWidth: 6,
-    borderLeftColor: Colors.light.specialBlue,
-    shadowColor: "#000",
-    shadowOpacity: 0.1,
-    shadowRadius: 5,
-    elevation: 3,
-    marginBottom: 10,
     alignItems: "center",
+    gap: 15,
   },
   image: {
     width: 70,
     height: 70,
     borderRadius: 6,
-    marginRight: 15,
-  },
-  infoContainer: {
-    flex: 1,
-  },
-  name: {
-    fontSize: 16,
-    fontWeight: "bold",
-  },
-  address: {
-    fontSize: 14,
-    color: "#666",
-  },
-  contact: {
-    fontSize: 14,
-    color: "#888",
   },
 });
