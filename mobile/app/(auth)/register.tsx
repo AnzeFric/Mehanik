1d2edb63032488d9b5b4d21e8f1f86d97de1dc04
AnzeFric <anzeifric@gmail.com>
2025-06-12 21:55:30 +0200

    Frontend, register: cleanup

--- mobile/app/(auth)/register.tsx
+++ mobile/app/(auth)/register.tsx
@@ -162,7 +162,7 @@ export default function LoginScreen() {
                 bold
                 style={{ color: staticColors.button }}
               >
-                Registracija
+                Prijava
               </ThemedText>
             </TouchableOpacity>
           </View>
