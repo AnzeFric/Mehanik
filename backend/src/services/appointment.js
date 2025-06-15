ba87ec28ea6bfbdd2b36132824658d0f52cd144b
AnzeFric <anzeifric@gmail.com>
2025-06-15 13:10:19 +0200

    Backend, appointment: Added service logic to fetch appointments from user

--- backend/src/services/appointment.js
+++ backend/src/services/appointment.js
@@ -2,7 +2,24 @@ const supabase = require("../config/database");
 const { v4: uuidv4 } = require("uuid");
 
 const appointmentService = {
-  async getAppointmentsByUserUuid(userUuid) {},
+  async getAppointmentsByUserUuid(userUuid) {
+    const { data, error } = await supabase
+      .from("appointments")
+      .select("uuid, date, status, user_message, mechanic_response")
+      .eq("fk_user", userUuid);
+
+    if (error) throw error;
+
+    const parsedData = (data || []).map((appointment) => ({
+      uuid: appointment.uuid,
+      date: appointment.date,
+      status: appointment.status,
+      userMessage: appointment.user_message,
+      mechanicResponse: appointment.mechanic_response,
+    }));
+
+    return parsedData;
+  },
 
   async saveAppointmentByUserUuid(userUuid, appointmentData) {},
 
