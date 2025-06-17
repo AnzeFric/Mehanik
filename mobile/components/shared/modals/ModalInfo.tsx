f89d4502112b9b9b317c9f8e885cca2e22e51ecb
AnzeFric <anzeifric@gmail.com>
2025-06-17 13:11:28 +0200

    Frontend, ModalInfo: Added ScrollView for displaying larger amount of content

--- mobile/components/shared/modals/ModalInfo.tsx
+++ mobile/components/shared/modals/ModalInfo.tsx
@@ -1,4 +1,4 @@
-import { View, Modal, StyleSheet } from "react-native";
+import { View, Modal, StyleSheet, ScrollView } from "react-native";
 import ThemedView from "@/components/global/themed/ThemedView";
 import ThemedText from "@/components/global/themed/ThemedText";
 import ThemedIcon from "@/components/global/themed/ThemedIcon";
@@ -24,39 +24,45 @@ export default function ModalInfo({
   return (
     <Modal transparent={true} visible={isVisible} onRequestClose={onCancel}>
       <View style={styles.centeredView}>
-        <ThemedView type={"primary"} style={styles.modalView}>
-          <View style={{ flexDirection: "row" }}>
-            <ThemedText type={"normal"} bold style={styles.title}>
-              {modalTitle}
-            </ThemedText>
-            <ThemedIcon
-              name={"close"}
-              size={30}
-              onPress={onCancel}
-              style={{ alignSelf: "flex-end" }}
-            />
-          </View>
-          <View style={styles.sectionContainer}>
-            {sections.map((section, index) => (
-              <ThemedView
-                type={"secondary"}
-                style={styles.sectionItem}
-                key={index}
-              >
-                <ThemedText type={"small"} bold>
-                  {section.title}
-                </ThemedText>
-                <View>
-                  {section.text.map((text, index) => (
-                    <ThemedText type={"extraSmall"} key={index}>
-                      {text}
-                    </ThemedText>
-                  ))}
-                </View>
-              </ThemedView>
-            ))}
-          </View>
-        </ThemedView>
+        <ScrollView
+          showsVerticalScrollIndicator={false}
+          style={styles.scrollContainer}
+          contentContainerStyle={styles.scrollContent}
+        >
+          <ThemedView type={"primary"} style={styles.container}>
+            <View style={{ flexDirection: "row" }}>
+              <ThemedText type={"normal"} bold style={styles.title}>
+                {modalTitle}
+              </ThemedText>
+              <ThemedIcon
+                name={"close"}
+                size={30}
+                onPress={onCancel}
+                style={{ alignSelf: "flex-end" }}
+              />
+            </View>
+            <View style={styles.sectionContainer}>
+              {sections.map((section, index) => (
+                <ThemedView
+                  type={"secondary"}
+                  style={styles.sectionItem}
+                  key={index}
+                >
+                  <ThemedText type={"small"} bold>
+                    {section.title}
+                  </ThemedText>
+                  <View>
+                    {section.text.map((text, index) => (
+                      <ThemedText type={"extraSmall"} key={index}>
+                        {text}
+                      </ThemedText>
+                    ))}
+                  </View>
+                </ThemedView>
+              ))}
+            </View>
+          </ThemedView>
+        </ScrollView>
       </View>
     </Modal>
   );
@@ -69,10 +75,17 @@ const styles = StyleSheet.create({
     alignItems: "center",
     backgroundColor: "rgba(0, 0, 0, 0.6)",
   },
-  modalView: {
-    borderRadius: 6,
-    width: "75%",
+  scrollContainer: {
+    width: "85%",
+    maxHeight: "80%",
+  },
+  scrollContent: {
+    flexGrow: 1,
+    justifyContent: "center",
+  },
+  container: {
     padding: 10,
+    borderRadius: 6,
   },
   title: {
     flex: 1,
