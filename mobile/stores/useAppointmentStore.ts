cd55230a33e423cb6cc88dec74f813dad4422c7f
AnzeFric <anzeifric@gmail.com>
2025-06-18 13:57:28 +0200

    Frontend: Added appointment store

---
+++ mobile/stores/useAppointmentStore.ts
@@ -0,0 +1,41 @@
+import { create } from "zustand";
+import { persist, createJSONStorage } from "zustand/middleware";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { UserAppointmentData } from "@/interfaces/appointment";
+
+interface AppointmentStore {
+  shouldRefetch: Boolean;
+  userAppointments: Array<UserAppointmentData>;
+  setShouldRefetch: (shouldRefetch: Boolean) => void;
+  setUserAppointments: (userAppointments: Array<UserAppointmentData>) => void;
+  reset: () => void;
+}
+
+const initialState = {
+  shouldRefetch: false,
+  userAppointments: [],
+};
+
+const useAppointmentStore = create(
+  persist<AppointmentStore>(
+    (set) => ({
+      ...initialState,
+      setShouldRefetch: (shouldRefetch: Boolean) => {
+        set({ shouldRefetch: shouldRefetch });
+      },
+      setUserAppointments: (userAppointments: Array<UserAppointmentData>) => {
+        set({ userAppointments: userAppointments });
+      },
+      reset: async () => {
+        set(() => ({ ...initialState }));
+        useAppointmentStore.persist.clearStorage();
+      },
+    }),
+    {
+      name: "appointmentStore",
+      storage: createJSONStorage(() => AsyncStorage),
+    }
+  )
+);
+
+export default useAppointmentStore;
