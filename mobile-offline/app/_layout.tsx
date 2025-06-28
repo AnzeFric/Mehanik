00e7de0c8f049bfb3359fe5a85034e20d55fe814
AnzeFric <anzeifric@gmail.com>
2025-06-28 12:53:12 +0200

    Frontend: Simplified app structure

--- mobile-offline/app/_layout.tsx
+++ mobile-offline/app/_layout.tsx
@@ -10,7 +10,24 @@ export default function RootLayout() {
         <ThemedStatusBar />
         <Stack>
           <Stack.Screen name="index" options={{ headerShown: false }} />
-          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="customer/add" options={{ headerShown: false }} />
+          <Stack.Screen
+            name="customer/[uuid]"
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="customer/edit/[uuid]"
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen name="repair/[uuid]" options={{ headerShown: false }} />
+          <Stack.Screen
+            name="repair/add/[uuid]"
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="repair/edit/[uuid]"
+            options={{ headerShown: false }}
+          />
           <Stack.Screen name="+not-found" options={{ headerShown: false }} />
         </Stack>
       </ThemeProvider>
